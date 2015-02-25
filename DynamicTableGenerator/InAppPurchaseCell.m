fda388e43959aa25917001ca5a298d19fb642a19
tpb <theobrown0@gmail.com>
2015-02-24 23:39:25 -0600

    updates

--- DynamicTableGenerator/InAppPurchaseCell.m
+++ DynamicTableGenerator/InAppPurchaseCell.m
@@ -21,18 +21,48 @@
     if (self) {
         
         self.buyButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        
       self.buyButton.translatesAutoresizingMaskIntoConstraints=NO;
-                          self.buyButton.frame=CGRectMake(0, 0, 40, 40);
-                          //        self.dateButon.backgroundColor = [UIColor colorWithRed:1 green:0 blue:0 alpha:0.5]; // light blue
-        [self.buyButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+                          self.buyButton.frame=CGRectMake(0, 0, 80, 80);
+        
+        // Set the button Text Color
+        [self.buyButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [self.buyButton setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
+        
+        // Set default backgrond color
+        [self.buyButton setBackgroundColor:[UIColor blackColor]];
+        // Draw a custom gradient
+        CAGradientLayer *btnGradient = [CAGradientLayer layer];
+        btnGradient.frame = self.buyButton.bounds;
+        btnGradient.colors = [NSArray arrayWithObjects:
+                              (id)[[UIColor colorWithRed:102.0f / 255.0f green:102.0f / 255.0f blue:102.0f / 255.0f alpha:1.0f] CGColor],
+                              (id)[[UIColor colorWithRed:51.0f / 255.0f green:51.0f / 255.0f blue:51.0f / 255.0f alpha:1.0f] CGColor],
+                              nil];
+        [self.buyButton.layer insertSublayer:btnGradient atIndex:0];
+        
+        // Round button corners
+        CALayer *btnLayer = [self.buyButton layer];
+        [btnLayer setMasksToBounds:YES];
+        [btnLayer setCornerRadius:5.0f];
+        
+        // Apply a 1 pixel, black border
+        [btnLayer setBorderWidth:1.0f];
+        [btnLayer setBorderColor:[[UIColor blackColor] CGColor]];
+        
+        
+        
+        
+//        self.dateButon.backgroundColor = [UIColor colorWithRed:1 green:0 blue:0 alpha:0.5]; // light blue
         [self.buyButton addTarget:self action:@selector(buyButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
 //        [self.buyButton addTarget:self action:@selector(contentWasSelected:) forControlEvents:UIControlEventTouchDown];
-        [self.buyButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-//        [self.buyButton setBackgroundColor:[UIColor blueColor]];
-        [self defineContentSelector:@selector(buyButtonPressed:)];
+
+
+//        [self defineContentSelector:@selector(buyButtonPressed:)];
         
         //set button text to display to the right, slightly offset from edge
-        self.buyButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentRight;
+        self.buyButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentCenter;
+        self.buyButton.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+
         self.buyButton.contentEdgeInsets = UIEdgeInsetsMake(0,0,0,10);
         [self.contentView addSubview:self.buyButton];
     }
@@ -54,8 +84,10 @@
             [self.buyButton autoSetContentCompressionResistancePriorityForAxis:ALAxisVertical];
         }];
         [self.buyButton autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:kLabelVerticalInsets];
+        [self.buyButton autoPinEdgeToSuperviewEdge:ALEdgeBottom withInset:kLabelVerticalInsets];
+
         [self.buyButton autoPinEdgeToSuperviewEdge:ALEdgeTrailing withInset:kLabelHorizontalInsets];
-        [self.buyButton autoPinEdge:ALEdgeLeft toEdge:ALEdgeRight ofView:self.title withOffset:kLabelHorizontalSpace];
+        [self.buyButton autoPinEdge:ALEdgeLeft toEdge:ALEdgeRight ofView:self.title withOffset:3*kLabelHorizontalSpace];
         
         self.didSetupAcessoryConstraints = YES;
     }
